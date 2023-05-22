8e8cda66fe3bbe2c310798557e8c45c452595fac
rhettb253 <rhettbeardemphl@hotmail.com>
2023-05-22 15:37:25 -0700

    update

---
+++ seed.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URL);
+
+const Book = require('./Model/book');
+
+//create a new Book and save it to DB
+async function seed() {
+    const book1 = new Book({
+        title: 'The farmer goes north',
+        description: 'After running out of good soil, a farmer has to make a decision to leave his village or risk starvation.',
+        status: '3 years old'
+    })
+
+    await book1.save()
+        .then(() => console.log('Saved book to the DB.'))
+        .catch((error) => console.error(error));
+
+    // alternate way to create a Book and save it to DB
+    await Book.create({
+        title: 'The farmer goes east',
+        description: 'After running out of good soil again, a farmer has to make a decision to leave his village or risk starvation.',
+        status: '2 years old'
+    })
+        .then(() => console.log('Saved book to the DB.'))
+        .catch((err) => console.error(err));
+
+    await Book.create({
+        title: 'The farmer goes south',
+        description: 'After running out of good soil for a third time, a farmer has to make a decision to leave his village or risk starvation.',
+        status: '1 year old'
+    })
+        .then(() => console.log('Saved book to the DB.'))
+        .catch((err) => console.error(err));
+
+    mongoose.disconnect();
+}
+
+seed();
--- server.js
+++ server.js
@@ -3,16 +3,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
+const PORT = process.env.PORT;
+mongoose.connect(process.env.MONGODB_URL);
+const db = mongoose.connection;
+// because mongoose is getting a Promise back from MongoDB we could also attach a .then and .catch to fire off a function or error on init
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', ()=> console.log('Mongoose is connected'));
 
-const PORT = process.env.PORT || 3001;
-
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
-})
+app.get('/', (request, response) => {
+  response.status(200).send('default route working');
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
